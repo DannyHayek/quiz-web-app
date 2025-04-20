518aaba0936cae6b6c8490163bb0993351b3442d
Danny <dhayek92@gmail.com>
2025-04-20 18:07:43 +0300

    Adapted title output on quiz page based on the currentQuiz selected, rest of output pending

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,7 +1,8 @@
 let quizData = JSON.parse(localStorage.getItem("quizData"));
-let curQuiz = quizData[localStorage.getItem("currentQuiz")];
+console.log(localStorage.getItem("currentQuiz"));
+let currentQuizID = parseInt(localStorage.getItem("currentQuiz").charAt(4)) - 1;
+console.log(currentQuizID);
+let curQuiz = quizData[currentQuizID];
 
 document.getElementById("quiz-name").innerHTML += `${curQuiz.topic}`;
-
-
 
