d4b7bcb8e4397b8b0091ad562345945823b8d284
Danny <dhayek92@gmail.com>
2025-04-20 14:20:43 +0300

    Created logic for logging in with several error checking protocols and messages, added favicon

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,56 @@ const login = document.getElementById("login");
 const register = document.getElementById("register");
 
 register.addEventListener("click", storeUser);
+login.addEventListener("click", loginUser);
 
 function storeUser() {
+    loginReadyOff();
+    infoMissingOff();
+    errorPassOff();
+    NAUserOff();
+
     const user = document.getElementById("username");
     const pass = document.getElementById("password");
 
     if (user.value == "" || pass.value == "") {
         console.log("You have missing data!");
-        loginReadyOff();
         infoMissingOn();
     } else {
         localStorage.setItem("username" , user.value);
         localStorage.setItem("password" , pass.value);
-        infoMissingOff();
         loginReadyOn();
+    }
+
+    user.value = "";
+    pass.value = "";
+
+    console.log(localStorage);
+}
+
+function loginUser() {
+    loginReadyOff();
+    infoMissingOff();
+    errorPassOff();
+    NAUserOff();
+
+
+    const user = document.getElementById("username");
+    const pass = document.getElementById("password");
+
+    if (user.value == "" || pass.value == "") {
+        console.log("You have missing data!");
+        infoMissingOn();
+    } else {
+        if (user.value == localStorage.getItem("username") && pass.value == localStorage.getItem("password")) {
+            console.log("LOGGED IN!")
+            
+        } else if (user.value == localStorage.getItem("username") && pass.value != localStorage.getItem("password")){
+            console.log("Incorrect password!")
+            errorPassOn();
+        } else {
+            console.log("USER DOES NOT EXIST! PLEASE REGISTER FIRST!")
+            NAUserOn();
+        }
     }
 
     user.value = "";
@@ -38,4 +74,20 @@ function loginReadyOn() {
 
 function loginReadyOff() {
     document.getElementById("ready-login").style.display = "none";
+}
+
+function errorPassOn() {
+    document.getElementById("error-pass").style.display = "block";
+}
+
+function errorPassOff() {
+    document.getElementById("error-pass").style.display = "none";
+}
+
+function NAUserOn() {
+    document.getElementById("na-user").style.display = "block";
+}
+
+function NAUserOff() {
+    document.getElementById("na-user").style.display = "none";
 }
