b8ddbefe4d5ae9f33715134d35e4cffe2368a59c
Danny <dhayek92@gmail.com>
2025-04-20 13:56:19 +0300

    Added an error message on authentication page for missing authen info

--- scripts/index.js
+++ scripts/index.js
@@ -6,16 +6,26 @@ register.addEventListener("click", storeUser);
 function storeUser() {
     const user = document.getElementById("username");
     const pass = document.getElementById("password");
-    
+
     if (user.value == "" || pass.value == "") {
         console.log("You have missing data!");
+        infoMissingOn();
     } else {
         localStorage.setItem("username" , user.value);
         localStorage.setItem("password" , pass.value);
+        infoMissingOff();
     }
 
     user.value = "";
     pass.value = "";
 
     console.log(localStorage);
+}
+
+function infoMissingOn() {
+    document.getElementById("missing-info").style.display = "block";
+}
+
+function infoMissingOff() {
+    document.getElementById("missing-info").style.display = "none";
 }
