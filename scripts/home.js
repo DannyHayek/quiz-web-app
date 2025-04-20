33c22583276aca5ed3f48c0b27fc0a149233d6b9
Danny <dhayek92@gmail.com>
2025-04-20 20:21:19 +0300

    Changed answer fields in quizData object to reflect actual answers as opposed to indices

--- scripts/home.js
+++ scripts/home.js
@@ -7,7 +7,7 @@ localStorage.setItem("quizData", JSON.stringify(
             description: "This quiz will test your knowledge of geography! Prepare to answer difficult questions about different points on our blue planet!",
             questions: ["How tall is Mount Everest (in meters)?", "How deep is the Mariana Trench (in meters)?", "What is the common name of the loneliest point on Earth?"],
             answers: [[7245, 8848, 9320, 10065], [5032, 7999, 12000, 10984], ["Point Nemo", "Point Dory", "Point Taha", "Point Posiedon"]],
-            correct: [3, 4, 1],
+            correct: [9320, 10984, "Point Nemo"],
         },
         {
             id: 1,
@@ -15,7 +15,7 @@ localStorage.setItem("quizData", JSON.stringify(
             description: "Over the last 100 years, the movie-making industry has been expanding and growing, meeting the demands of ever changing audience interest. Can you answer these movie related questions?",
             questions: ["What is the highest grossing movie of all time?", "What is the highest grossing movie when adjusted for inflation?", "In what year did Back to the Future release?"],
             answers: [["Avengers: Endgame", "Titanic", "Avatar", "Avatar: The Way of Water"], ["Avatar", "Star Wars", "Gone with the Wind", "Titanic"], [1990, 1985, 1970, 2001]],
-            correct: [3, 3, 2],
+            correct: ["Avatar", "Gone with the Wind", 1985],
         },
         {
             id: 2,
@@ -23,7 +23,7 @@ localStorage.setItem("quizData", JSON.stringify(
             description: "Throughout history, humanity has persisted utop the shoulders of teachers and instructors. Some stood out amongst their peers as the greatest, having taught the greatest.",
             questions: ["Who was the teacher of Alexander the Great?", "Who was Plato's teacher?", "Who is the greatest instructor?"],
             answers: [["Aristotle", "Plato", "Pythagoras", "Einstein"], ["Voltaire", "Sun Tzu", "Euclides", "Socrates"], ["Taha Not Taha", "Hata Hata", "Thaa Aath", "Taha Taha"]],
-            correct: [1, 4, 4],
+            correct: ["Aristotle", "Socrates", "Taha Taha"],
         },
         {
             id: 3,
@@ -31,7 +31,7 @@ localStorage.setItem("quizData", JSON.stringify(
             description: "Computers are complicated machines, with many different parts, each responsible for a separate function. Most people struggle to identify even the main parts, and it is easy to get confused betweent them.",
             questions: ["Which part is considered the short term memory?", "Which part is the backbone of most of the other parts?", "Which of these is not a real part?"],
             answers: [["RAM", "CPU", "Motherboard", "Ethernet Port"], ["PSU", "Motherboard", "Heat Sink", "GPU"], ["LOL", "RAM", "CPU", "HDD"]],
-            correct: [1, 2, 1],
+            correct: ["RAM", "Motherboard", "LOL"],
         }
     ]
 ));
